471f8b984befb72236dd0a6c04dad0f61dfffa93
MarianoIDC <mmunoz@estudiantec.cr>
2022-05-05 19:05:39 -0600

    Update algoritmo.py

--- PythonFiles/algoritmo.py
+++ PythonFiles/algoritmo.py
@@ -1,9 +1,10 @@
 import numpy as np
+import pandas
 
 
 def algoritmo():
     l = np.array([[10, 20],[30,40]])
-    m = 6
+    m = 4
     n = m-1
     a = np.zeros((m,m), int)
     a[0,0] = 10
@@ -15,13 +16,15 @@ def algoritmo():
     #generando vertical izquierda
     for i in range(1,n):
         index = i+1
-        a[i,0] = round(((((m-index)/(m-1))*a[0,0])+(((index-1)/(m-1))*a[n,0])))
+        # a[i,0] = round(((((m-index)/(m-1))*a[0,0])+(((index-1)/(m-1))*a[n,0])))
+        a[i,0] = ((((m-index)/(m-1))*a[0,0])+(((index-1)/(m-1))*a[n,0]))
         print(">>>>>>>>>>>>>>>>>>>>>>>")
         print(a)
     #generando vertical derecha
     for i in range(1,n):
         index = (i+1)*m
-        a[i,n] = round(((((m*m-index)/(m*m-m))*a[0,n])+(((index-m)/(m*m-m))*a[n,n])))
+        # a[i,n] = round(((((m*m-index)/(m*m-m))*a[0,n])+(((index-m)/(m*m-m))*a[n,n])))
+        a[i,n] = ((((m*m-index)/(m*m-m))*a[0,n])+(((index-m)/(m*m-m))*a[n,n]))
         print(">>>>>>>>>>>>>>>>>>>>>>>")
         print(a)
 
@@ -29,7 +32,8 @@ def algoritmo():
     for x in range(m):
         for y in range(1,n):
             index = y+1
-            a[x,y] = round(((((m-index)/(m-1))*a[x,0])+(((index-1)/(m-1))*a[x,n])))
+            # a[x,y] = round(((((m-index)/(m-1))*a[x,0])+(((index-1)/(m-1))*a[x,n])))
+            a[x,y] = ((((m-index)/(m-1))*a[x,0])+(((index-1)/(m-1))*a[x,n]))
             print(">>>>>>>>>>>>>>>>>>>>>>>")
             print(a)
 
