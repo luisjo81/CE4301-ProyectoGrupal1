67d5659f716ce9c1c98e1bf9dc788db3f14cba54
MarianoIDC <mmunoz@estudiantec.cr>
2022-05-07 22:49:59 -0600

    Udptade gitignore

--- decoInst.sv
+++ decoInst.sv
@@ -1,7 +1,14 @@
-module decoInst (input logic inst, output logic opcode);
-$display("Mariano")
+module decoInst (input logic clk, input logic [25:0] inst, output logic opcode);
 
 
-
-
+always @ (posedge clk)
+	begin
+	//$display("Instruction: %b", inst[25:24]);
+	case (inst[25:24])
+		2'b00 : $display("operacion de tipo Logico-Aritmetica"); 
+		2'b01 : $display("operacion de tipo Memoria");
+		2'b10 : $display("operacion de tipo Registro");
+		2'b11 : $display("operacion de tipo Branch");
+	endcase
+	end
 endmodule
--- tb_decoInst.sv
+++ tb_decoInst.sv
@@ -1,16 +1,18 @@
 module tb_decoInst();
 
 logic [25:0] inst;
-clk = 0;
+logic opcode;
+logic clk = 0;
 
-decoInst decoInst (.)
+decoInst decoInst (.clk(clk), .inst(inst), .opcode(opcode));
+
+always #10 clk = ~clk;
 
 initial
 begin
 
-
+inst = 26'b00000000000000000000000011;
 
 end
-
 
 endmodule 
