28588e62ccbcdc229464485652995ff1c3afd4fc
MarianoIDC <mmunoz@estudiantec.cr>
2022-05-08 12:00:23 -0600

    Update

--- decoInst.sv
+++ decoInst.sv
@@ -1,14 +1,69 @@
-module decoInst (input logic clk, input logic [25:0] inst, output logic opcode);
+module decoInst (
+//Entradas:
+	input logic clk, 
+	input logic [25:0] inst, 
+//Salidas: 
+	output logic [5:0] opcode,
+	output logic [4:0] rd ,
+	output logic [4:0] rn ,
+	output logic [4:0] rm ,
+	output logic [9:0] imm10, 
+	output logic [14:0] imm15, 
+	output logic [19:0] imm20
+	
+	);
 
 
 always @ (posedge clk)
 	begin
 	//$display("Instruction: %b", inst[25:24]);
 	case (inst[25:24])
-		2'b00 : $display("operacion de tipo Logico-Aritmetica"); 
-		2'b01 : $display("operacion de tipo Memoria");
-		2'b10 : $display("operacion de tipo Registro");
-		2'b11 : $display("operacion de tipo Branch");
+		2'b00 : 
+			begin
+				$display("operacion de tipo Logico-Aritmetica");
+				opcode = inst[25:20] ;
+				rd = inst[19:15] ;
+				rn = inst[14:10] ;
+				case (inst[23])
+					1'b0 : 
+					begin
+						$display("::::::operacion de registros");
+						rm = inst[9:5] ;
+					end
+					1'b1 : 
+					begin
+						$display("::::::operacion de inmediatos");
+						imm10 = inst[9:0] ;
+					end
+				endcase
+			end
+		2'b01 :
+			begin	
+				$display("operacion de tipo Memoria");
+				opcode = inst[25:20] ;
+				rd = inst[19:15] ;
+				rn = inst[14:10] ;
+				case (inst[23])
+				1'b0 : $display("::::::operacion de registros");
+				1'b1 : $display("::::::operacion de inmediatos");
+				endcase
+			end
+		2'b10 :
+			begin
+				$display("operacion de tipo Registro");
+				case (inst[23])
+				1'b0 : $display("::::::operacion de registros");
+				1'b1 : $display("::::::operacion de inmediatos");
+				endcase
+			end
+		2'b11 :
+			begin
+				$display("operacion de tipo Branch");
+				case (inst[23])
+				1'b0 : $display("::::::operacion de registros");
+				1'b1 : $display("::::::operacion de inmediatos");
+				endcase
+			end
 	endcase
 	end
 endmodule
--- tb_decoInst.sv
+++ tb_decoInst.sv
@@ -3,16 +3,34 @@ module tb_decoInst();
 logic [25:0] inst;
 logic opcode;
 logic clk = 0;
+logic [4:0] rd;
+logic [4:0] rn;
+logic [4:0] rm;
+logic [9:0] imm10; 
+logic [14:0] imm15; 
+logic [19:0] imm20;
 
-decoInst decoInst (.clk(clk), .inst(inst), .opcode(opcode));
+decoInst decoInst (
+					.clk(clk), 
+					.inst(inst), 	
+					.opcode(opcode), 
+					.rd(rd),
+					.rn(rn),
+					.rm(rm),
+					.imm10(imm10), 
+					.imm15(imm15), 
+					.imm20(imm20));
 
-always #10 clk = ~clk;
+always #5 clk = ~clk;
 
 initial
-begin
+	begin
 
-inst = 26'b00000000000000000000000011;
+	inst = 26'b00000000000000000000000011; #10;
+	inst = 26'b01000000000000000000000011; #10;
+	inst = 26'b11000000000000000000000011; #10;
+	inst = 26'b10000000000000000000000011; #10;
+	inst = 26'b10000000000000000000000011; #10;
 
-end
-
+	end
 endmodule 
