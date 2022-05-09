97a69215bc6722f30255f736aa71ac8172e91aaf
MarianoIDC <mmunoz@estudiantec.cr>
2022-05-08 23:05:44 -0600

    Update procesador.sv

--- procesador.sv
+++ procesador.sv
@@ -1,6 +1,16 @@
-module procesador()
+module procesador(input logic clk);
 
 
 
 
+
+//Modulo del PC
+
+//Modulo de las instrucciones
+
+//:::: 
+
+//Modulo del decodificador
+decoInst decoInst_m (.clk(clk), .inst(inst), .opcode(opcode), .rd(rd), .rn(rn), 
+							.rm(rm), .imm10(imm10), .imm15(imm15), .imm20(imm20));
 endmodule
