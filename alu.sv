c76e6a7251d1b6e837cd4f6866f2c2378818b714
Kevin González <13704171+kgonzalez712@users.noreply.github.com>
2022-05-08 22:51:55 -0600

    ALU y Sumador
    Operaciones ALU y modulo sumador como plus

--- alu.sv
+++ alu.sv
@@ -16,6 +16,15 @@ module alu #(parameter N=32)
 	logic [N-1:0]temp_result_or;
 	logic [N-1:0]temp_result_xor;
 	logic [N-1:0]temp_result_not;
+	logic [N-1:0]temp_result_ldr;
+	logic [N-1:0]temp_result_str;
+	logic [N-1:0]temp_result_je;
+	logic [N-1:0]temp_result_jne;
+	logic [N-1:0]temp_result_jgt;
+	logic [N-1:0]temp_result_jge;
+	logic [N-1:0]temp_result_jlt;
+	logic [N-1:0]temp_result_jle;
+	
 
 // Flags temporales - temp flags
 /*
@@ -38,6 +47,15 @@ module alu #(parameter N=32)
 	or_op OR_OP(src_A, src_B, temp_result_or);//OR
 	xor_op XOR_OP(src_A, src_B, temp_result_xor);//XOR
 	not_op NOT_OP(src_A, temp_result_not);//NOT
+	//MEM ?
+	add_op LDR_OP(src_A, src_B, temp_result_ldr); // ADD LDR
+	add_op STR_OP(src_A, src_B, temp_result_str); // ADD STR
+	je_op JE_OP(src_A, temp_result_je);//JEQ
+	jne_op JNE_OP(src_A, temp_result_jne);//JNE
+	jgt_op JGT_OP(src_A, temp_result_jgt);//JGT
+	jge_op JGE_OP(src_A, temp_result_jge);//JGE
+	jlt_op JLT_OP(src_A, temp_result_jlt);//JLT
+	jle_op JLE_OP(src_A, temp_result_jle);//JLE
 	
 	
 	
@@ -80,10 +98,37 @@ module alu #(parameter N=32)
 				   TRESULT = temp_result_not;
 				  end
 				  
-			/* 17 : begin 
-				   TRESULT = temp_result_load;
+			 17 : begin 
+				   TRESULT = temp_result_ldr;
 				  end	
-			*/
+				  				  
+			 19 : begin 
+				   TRESULT = temp_result_str;
+				  end	
+			
+			 25 : begin 
+				   TRESULT = temp_result_je;
+					end
+					
+			 26 : begin 
+				   TRESULT = temp_result_jqe;
+					end
+					
+			 27 : begin 
+				   TRESULT = temp_result_jgt;
+					end
+			
+			 28 : begin 
+				   TRESULT = temp_result_jge;
+					end
+					
+			 29 : begin 
+				   TRESULT = temp_result_jlt;
+					end
+					
+			 30 : begin 
+				   TRESULT = temp_result_jle;
+					end
 
 			 default : begin 
 				   TRESULT = 32'd0;
