4af343fe3a11d3fe9b17c06196e051a8f97d44fb
MarianoIDC <mmunoz@estudiantec.cr>
2022-05-08 20:50:34 -0600

    Deco completo

--- decoInst.sv
+++ decoInst.sv
@@ -20,7 +20,7 @@ always @ (posedge clk)
 	case (inst[25:24])
 		2'b00 : 
 			begin
-				$display("operacion de tipo Logico-Aritmetica");
+				$display("operacion de tipo Aritmetica");
 				opcode = inst[25:20] ;
 				rd = inst[19:15] ;
 				rn = inst[14:10] ;
@@ -39,29 +39,55 @@ always @ (posedge clk)
 			end
 		2'b01 :
 			begin	
+				$display("operacion de tipo Logicas");
+				opcode = inst[25:20] ;
+				rd = inst[19:15] ;
+				rn = inst[14:10] ;
+				case (inst[23])
+				1'b0 : 
+					begin
+						$display("::::::operacion de registros");
+						rm = inst[9:5] ;
+					end
+					1'b1 : 
+					begin
+						$display("::::::operacion de inmediatos");
+						imm10 = inst[9:0] ;
+					end
+				endcase
+			end
+		2'b10 :
+			begin
 				$display("operacion de tipo Memoria");
 				opcode = inst[25:20] ;
 				rd = inst[19:15] ;
 				rn = inst[14:10] ;
 				case (inst[23])
-				1'b0 : $display("::::::operacion de registros");
-				1'b1 : $display("::::::operacion de inmediatos");
-				endcase
-			end
-		2'b10 :
-			begin
-				$display("operacion de tipo Registro");
-				case (inst[23])
-				1'b0 : $display("::::::operacion de registros");
+				1'b0 : 
+				begin
+					$display("::::::operacion de registros");
+					imm10 = inst[9:0];
+				end
 				1'b1 : $display("::::::operacion de inmediatos");
 				endcase
 			end
 		2'b11 :
 			begin
 				$display("operacion de tipo Branch");
+				opcode = inst[25:20] ;
 				case (inst[23])
-				1'b0 : $display("::::::operacion de registros");
-				1'b1 : $display("::::::operacion de inmediatos");
+				1'b0 : 
+				begin
+					$display("::::::operacion de con comparacion");
+					rd = inst[19:15] ;
+					rn = inst[14:10] ;
+					imm10 = inst[9:0];
+				end
+				1'b1 : 
+				begin
+					$display("::::::operacion de sin	comparacion");
+					imm20 = inst[19:0];
+				end
 				endcase
 			end
 	endcase
