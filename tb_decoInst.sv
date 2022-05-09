e4bf9d0f6554aca184486183a5bfc01b6f879825
Java-Sith <prieto.luisdaniel@gmail.com>
2022-05-09 03:58:08 -0600

    Se ejecutan pruebas del pipeline

--- tb_decoInst.sv
+++ tb_decoInst.sv
@@ -3,7 +3,7 @@
 
 module tb_decoInst();
 
-logic [25:0] inst;
+logic [25:0] inst, inst_new;
 logic [5:0]opcode;
 logic clk = 0, rst, en = 1'b1;
 logic [4:0] rd;
@@ -12,28 +12,15 @@ logic [4:0] rm;
 logic [9:0] imm10; 
 logic [14:0] imm15; 
 logic [19:0] imm20;
-logic [15:0] pc_count, new_pc;
+logic [15:0] pc_count, new_pc, pc_count_new;
 
-decoInst decoInst (
-					.clk(clk), 
-					.inst(inst), 	
-					.opcode(opcode), 
-					.rd(rd),
-					.rn(rn),
-					.rm(rm),
-					.imm10(imm10), 
-					.imm15(imm15), 
-					.imm20(imm20));
+decoInst decoInst (clk, inst_new, opcode, rd, rn, rm, imm10, imm15, imm20);
 					
-Rom mem(pc_count,
-		clk,
-		inst);
+Rom mem(pc_count, clk, inst);
 		
-pc cont(.clk(clk),
-		.rst(rst),
-		.en(en),
-		.new_pc(new_pc),
-		.pc_count(pc_count));
+pc cont(clk, rst, en, new_pc, pc_count);
+		
+Pipeline_IF_ID pipelineFetch(clk, rst, inst, pc_count, inst_new, pc_count_new);
 
 always #5 clk = ~clk;
 
@@ -41,27 +28,18 @@ initial
 	begin
 	
 		rst = 1'b1; new_pc = 16'b0; #10;
-		rst = 1'b0; new_pc = new_pc + 16'b100; #10;
-		rst = 1'b0; new_pc = new_pc + 16'b100; #10;
-		rst = 1'b0; new_pc = new_pc + 16'b100; #10;
-		rst = 1'b0; new_pc = new_pc + 16'b100; #10;
-		rst = 1'b0; new_pc = new_pc + 16'b100; #10;
-		rst = 1'b0; new_pc = new_pc + 16'b100; #10;
-		rst = 1'b0; new_pc = new_pc + 16'b100; #10;
-		rst = 1'b0; new_pc = new_pc + 16'b100; #10;
-		rst = 1'b0; new_pc = new_pc + 16'b100; #10;
-		rst = 1'b0; new_pc = new_pc + 16'b100; #10;
-		rst = 1'b0; new_pc = new_pc + 16'b100; #10;
-		rst = 1'b0; new_pc = new_pc + 16'b100; #10;
-		rst = 1'b0; new_pc = new_pc + 16'b100; #10;
-		rst = 1'b0; new_pc = new_pc + 16'b100; #10;
-		rst = 1'b0; new_pc = new_pc + 16'b100; #10;
-		rst = 1'b0; new_pc = new_pc + 16'b100; #10;
-		rst = 1'b0; new_pc = new_pc + 16'b100; #10;
-		rst = 1'b0; new_pc = new_pc + 16'b100; #10;
-		rst = 1'b0; new_pc = new_pc + 16'b100; #10;
-		rst = 1'b0; new_pc = new_pc + 16'b100; #10;
-		rst = 1'b0; new_pc = new_pc + 16'b100; #10;
+		rst = 1'b0; new_pc = new_pc + 16'b1; #10;
+		rst = 1'b0; new_pc = new_pc + 16'b1; #10;
+		rst = 1'b0; new_pc = new_pc + 16'b1; #10;
+		rst = 1'b0; new_pc = new_pc + 16'b1; #10;
+		rst = 1'b0; new_pc = new_pc + 16'b1; #10;
+		rst = 1'b0; new_pc = new_pc + 16'b1; #10;
+		rst = 1'b0; new_pc = new_pc + 16'b1; #10;
+		rst = 1'b0; new_pc = new_pc + 16'b1; #10;
+		rst = 1'b0; new_pc = new_pc + 16'b1; #10;
+		rst = 1'b0; new_pc = new_pc + 16'b1; #10;
+		rst = 1'b0; new_pc = new_pc + 16'b1; #10;
+		rst = 1'b0; new_pc = new_pc + 16'b1; #10;
 
 	end
 endmodule 
