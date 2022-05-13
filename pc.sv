934f11667c1c62de9fc8d41dc0bebfd574746662
MarianoIDC <mmunoz@estudiantec.cr>
2022-05-12 20:31:13 -0600

    Update pc.sv
    Se agrega la recepcion del pc branch

--- pc.sv
+++ pc.sv
@@ -5,16 +5,20 @@ module pc #(parameter N=16)
 	input logic clk,
 	input logic rst,
 	input logic en,
-	input logic [N-1:0] new_pc,
+	input logic [N-1:0] pc_branch,
 	output logic [N-1:0] pc_count
 );
 
+//logic [N-1:0] pc_aux;
 
 		
 		always_ff @ (negedge clk or posedge rst)
 			if(rst) pc_count = 16'b0;
 			else
-//				if(en) pc_count = new_pc;
-				pc_count = pc_count+1;
+			begin
+				if(en) pc_count = pc_branch;
+				else pc_count = pc_count+1;
+			end
+
 	
 endmodule
