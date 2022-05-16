98da678940cd159a87e054a8c72452579bf36597
Luis Daniel Prieto <prieto.luisdaniel@gmail.com>
2022-05-15 22:10:25 -0600

    Se cargan datos en la memoria

--- PythonFiles/cargarImagen2.py
+++ PythonFiles/cargarImagen2.py
@@ -4,7 +4,7 @@ import numpy as np
 import cv2
 
 imagen = cv2.imread('./test.png', 0)
-cv2.imwrite('Grises.png',imagen)
+cv2.imwrite('Grises.png', imagen)
 
 image = Image.open("./Grises.png")
 
@@ -25,7 +25,7 @@ print(data.shape)
 
 
 print(data)
-np.savetxt(fname="imagenBinarizada.txt", X= data, delimiter=",", newline="\n", fmt="%.3d")
+np.savetxt(fname="imagenBinarizada.txt", X=data, delimiter=",", newline="\n", fmt="%.3d")
 
 # data.tofile(fid="./imagenBinarizada.txt")
 
@@ -33,21 +33,31 @@ np.savetxt(fname="imagenBinarizada.txt", X= data, delimiter=",", newline="\n", f
 
 intToBinary = lambda x, n: format(x, 'b').zfill(n)
 
+
 def imgToMemory(data):
     n = len(data)
-    filewrite = open("..\Ram_data.mif", "w")
+    filewrite = open("../Ram_data.mif", "w")
     filewrite.write("WIDTH=32;\n")
     filewrite.write("DEPTH=65536;\n")
     filewrite.write("ADDRESS_RADIX=UNS;\n")
     filewrite.write("DATA_RADIX=UNS;\n")
     filewrite.write("CONTENT BEGIN\n")
     mem_count = 0
-    for i in range(n * n):
-        byteData = intToBinary(data.item(i), 8)
-        filewrite.write("    "+str(mem_count)+"    :    "+str(byteData)+";\n")
-        mem_count+=1
-    filewrite.write("    ["+str(mem_count)+"..65535]    :    0;\n")
+    data_count = 0
+    while data_count < n * n - n:
+        byteOne = intToBinary(data.item(data_count), 8)
+        byteTwo = intToBinary(data.item(data_count + 1), 8)
+        byteThree = intToBinary(data.item(data_count + n), 8)
+        byteFour = intToBinary(data.item(data_count + (n + 1)), 8)
+        byteData = str(byteOne) + str(byteTwo) + str(byteThree) + str(byteFour)
+        filewrite.write("    " + str(mem_count) + "    :    " + byteData + ";\n")
+        mem_count += 1
+        if data_count == n - 1:
+            data_count += n
+        else:
+            data_count += 2
+    filewrite.write("    [" + str(mem_count) + "..65535]    :    0;\n")
     filewrite.write("END;\n")
 
-imgToMemory(data)
 
+imgToMemory(data)
